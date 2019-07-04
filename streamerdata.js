82d3eac5d394fc6d41bada3191a569e95abf222b
mexx16 <mexx.designz@gmail.com>
2019-07-04 22:32:45 +0200

    Add files via upload

---
+++ streamerdata.js
@@ -0,0 +1,14 @@
+/**MobilBarát Menetrend Testreszabása. */
+/*Streamer adatok megadása*/
+
+//A twitch neved amivel a csatorna elérhető --> http://twitch.tv/twitchneved.
+var streamer = "blyyyplays";
+//Kép megjelenítése amikor üres a menetrend.
+var noEventsPic = "blyyy_logo.png";
+//Kép megjelenítése amikor offline a menetrend(nembiztos, hogy megjelenik majd offline állapotban)
+var offlinePic = "blyyy_logo.png";
+//Offline állapotban megjelenő szöveg
+var offlineText = "Internet! Gyere Vissza! Kám Kám Kám!"; 
+//Üres menetrendkor megjelenő szöveg
+var noEventsText = "Menetrend Jelenleg üres!";
+
